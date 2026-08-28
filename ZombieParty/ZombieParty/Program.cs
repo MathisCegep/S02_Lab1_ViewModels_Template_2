12e7f7b8883ff9b9644f00d11613fae7319a6cb4
MathisCegep <2565641@cegepmontpetit.ca>
2026-08-28 09:47:32 -0400

    FCT ZombieType utilisation du singleton
    Ajout de l’utilisation du singleton BaseDonnees pour ZombieType.

--- ZombieParty/ZombieParty/Program.cs
+++ ZombieParty/ZombieParty/Program.cs
@@ -18,10 +18,6 @@ builder.Services.AddSingleton<BaseDonnees>();
 var app = builder.Build();
 
 
-
-
-var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
