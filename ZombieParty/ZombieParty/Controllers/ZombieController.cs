576d77d421811c1dcb2269c515542a2ddf5c1c6d
MathisCegep <2565641@cegepmontpetit.ca>
2026-08-28 10:25:51 -0400

    FCT Zombie Index et Create et Validation avec messages d’erreurs
    Maintenant possible de voir la liste de zombies et d’en ajouter avec gestion d’erreurs.
    5. Cliquez sur Commit

--- ZombieParty/ZombieParty/Controllers/ZombieController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieController.cs
@@ -1,15 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ZombieParty.Models;
 
 namespace ZombieParty.Controllers
 {
     public class ZombieController : Controller
     {
+        private BaseDonnees _baseDonnees { get; set; }
+
+        public ZombieController(BaseDonnees baseDonnees)
+        {
+            _baseDonnees = baseDonnees;
+        }
+
         public IActionResult Index()
         {
-            this.ViewBag.MaListe = new List<Zombie>();
+            this.ViewBag.MaListe = _baseDonnees.Zombies.ToList();
 
             return View();
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            ViewBag.ZombieTypes = new SelectList(_baseDonnees.ZombieTypes.ToList(), "Id", "TypeName", null);
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult Create(Zombie zombie)
+        {
+            if (ModelState.IsValid)
+            {
+                _baseDonnees.Zombies.Add(zombie);
+                return this.RedirectToAction("Index");
+            }
+
+            // set le zombie type du nouveau zombie
+            ZombieType zombieTypeSelectionee = _baseDonnees.ZombieTypes.Where(zt => zt.Id == zombie.ZombieTypeId).SingleOrDefault();
+            zombie.ZombieType = zombieTypeSelectionee;
+
+            ViewBag.ZombieTypes = new SelectList(_baseDonnees.ZombieTypes.ToList(), "Id", "TypeName", zombieTypeSelectionee);
+
+            return View(zombie);
         }
     }
 }
--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ZombieParty.Models;
 
 namespace ZombieParty.Controllers
